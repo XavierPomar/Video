30a3788122d1a189a945990fd3e977b3092c8562
Xavier Pomar <xpomar12442@alumnes.iesmanacor.cat>
2019-01-23 09:52:03 +0100

    Primer Canvi

---
+++ Schaum_3/src/schaum_3/Alumno.java
@@ -0,0 +1,14 @@
+package schaum_3;
+
+enum Horario {MAÑANA, TARDE}
+        
+public class Alumno {
+    private String nombre;
+    private String apellidos;
+    private int añoDeNacimiento;
+    
+    private int NP;
+    private String grupo;
+    private Horario horario;
+    //hola
+}
