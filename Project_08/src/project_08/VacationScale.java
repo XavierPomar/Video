2f32b69a509afbdff64572b58af78d671b12f851
Usuario <Usuario@DESKTOP-J8FQHOR>
2019-02-06 09:14:34 +0100

    Primer Commit

---
+++ Project_08/src/project_08/VacationScale.java
@@ -0,0 +1,28 @@
+package project_08;
+
+public class VacationScale {
+
+    int [] vacationDays;
+    int yearsOfService;
+    
+    public void setVacationScale (){
+    vacationDays = new int[7];
+    vacationDays[0] = 10;
+    vacationDays[1] = 15;
+    vacationDays[2] = 15;
+    vacationDays[3] = 15;
+    vacationDays[4] = 20;
+    vacationDays[5] = 20;
+    vacationDays[6] = 25;
+    }
+    
+     public void displayVacationDays (){
+     if (yearsOfService < 0) {
+     System.out.println("Numero de anys invalid");
+     } else if (yearsOfService <= 6){
+         System.out.println("Dies de vacacions: " + vacationDays[yearsOfService]);
+     } else {
+         System.out.println("Dies de vacacions: " + vacationDays[6]);
+     }
+   }   
+}
